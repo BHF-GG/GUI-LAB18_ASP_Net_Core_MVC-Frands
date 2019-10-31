2be7851a7b93c68d605bff2ef6aa62948308f1e3
FrandsAndersen <frands1995@gmail.com>
2019-10-31 09:26:58 +0100

    Add project files.

---
+++ LAB18_ASP_Net_Core_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB18_ASP_Net_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
